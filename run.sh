226b873ac282ae3a6dcc7fd4dd57474231ac325b
root <root@bolt-cjqhdxc9mt-jb2237kkiz.bolt-pods.turi-bolt.svc.cluster.local>
2024-11-29 10:55:26 +0000

    support mmdit like arch

--- run.sh
+++ run.sh
@@ -10,7 +10,10 @@ python train.py --config-dir=config --config-name=image_pusht_diffusion_policy_t
 
 ## LowDim training config
 # Transformer
-# python train.py --config-dir=config --config-name=lowdim_pusht_dp_tf_20pc training.seed=42 training.device=cuda:0 hydra.run.dir='data/outputs/${now:%Y.%m.%d}/${now:%H.%M.%S}_${name}_${task_name}'
+# python train.py --config-dir=config --config-name=lowdim_pusht_diffusion_policy_tf.yaml training.seed=42 training.device=cuda:0 hydra.run.dir='data/outputs/${now:%Y.%m.%d}/${now:%H.%M.%S}_${name}_${task_name}'
+
+python train.py --config-dir=config --config-name=lowdim_pusht_diffusion_policy_tf.yaml training.seed=42 training.device=cuda:1 hydra.run.dir='data/outputs/${now:%Y.%m.%d}/${now:%H.%M.%S}_${name}_${task_name}'
+
 
 # CNN
 # python train.py --config-dir=config --config-name=lowdim_pusht_diffusion_policy_cnn.yaml training.seed=42 training.device=cuda:0 hydra.run.dir='data/outputs/${now:%Y.%m.%d}/${now:%H.%M.%S}_${name}_${task_name}'
